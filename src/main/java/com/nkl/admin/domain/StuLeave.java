4427179654b0952e77e6d21eb8c512b554234b85
liuhaide <1057834465@qq.com>
2021-03-01 09:47:58 +0800

    commit

---
+++ src/main/java/com/nkl/admin/domain/StuLeave.java
@@ -0,0 +1,69 @@
+package com.nkl.admin.domain;
+
+public class StuLeave {
+	private int id;
+	
+	private String reason;
+	
+	private int flag;
+	
+	private int state;
+	
+	private String piid;
+	
+	private String stuname;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public int getFlag() {
+		return flag;
+	}
+
+	public void setFlag(int flag) {
+		this.flag = flag;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public String getPiid() {
+		return piid;
+	}
+
+	public void setPiid(String piid) {
+		this.piid = piid;
+	}
+
+	public String getStuname() {
+		return stuname;
+	}
+
+	public void setStuname(String stuname) {
+		this.stuname = stuname;
+	}
+	
+	
+	
+	
+	
+
+}
